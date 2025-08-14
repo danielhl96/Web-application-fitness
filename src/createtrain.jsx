bfca6073fca37f025afbec713b8b0007788e6b44
Daniel Hennies <daniel.hennies@stud.uni-hannover.de>
2025-08-14 13:53:40 +0200

    createtraining

--- src/createtrain.jsx
+++ src/createtrain.jsx
@@ -1,9 +1,57 @@
-import './index.css'; 
+import './index.css';
+import Header  from './Header'
 
-function createTrain() {
+
+
+function Exercise_cards(name,text){
     return (
-<div></div>
+        <div className="card w-96 bg-base-100 card-xs shadow-sm">
+  <div className="card-body">
+    <h2 className="card-title">{name}</h2>
+    <figure>
+    <img
+      src="./public/fitnessstudio.png"
+      width="75" height="75"/>
+  </figure>
+    <p>{text}</p>
+    <div className="justify-end card-actions">
+      <button className="btn btn-secondary">Remove</button>
+    </div>
+  </div>
+</div>
+    )
+}
 
+function CreateTrainGUI() {
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center gap-6">
+        <Header/>
+        <p className='text-4xl'> Create your training:</p>
+        <label className="input">
+            <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"  
+                    strokeWidth="2.5" 
+                    fill="none"
+                    stroke="currentColor"
+                >
+                    <circle cx="11" cy="11" r="8"></circle>
+                    <path d="m21 21-4.3-4.3"></path>
+                </g>
+            </svg>
+            <input type="search" className="grow" placeholder="Select your exercise" />
+        </label>
+
+        {Exercise_cards("Bench press","A exercise to train the chest, shoulder and triceps")}
+
+        <div class="flex flex-row">
+        <button className="btn btn-outline btn-primary">Save</button>
+        <button className="btn btn-outline btn-secondary">Cancel</button>
+        </div>
+        </div>
+       
     );
 }
 
+export default CreateTrainGUI;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import GUI from './login.jsx'
-
+import CreateTrainGUI from './createtrain.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <GUI/>
+  <CreateTrainGUI />
   </StrictMode>,
 )
