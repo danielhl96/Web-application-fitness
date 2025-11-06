6570689a9df4ea496aca0a84ec0f8e4d199235b9
Daniel Hennies <daniel.hennies@stud.uni-hannover.de>
2025-11-06 21:09:18 +0100

    PWA

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./AuthProvider.jsx";
-
+import { registerSW } from "virtual:pwa-register";
+registerSW({});
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Fitness App",
+        short_name: "Fitness",
+        description: "Fitness App to record and create your workouts",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "./squats.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "./squats.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     host: "0.0.0.0", // <-- Neu: Erlaubt Zugriff über Netzwerk (nicht nur localhost)
     port: 5173,
