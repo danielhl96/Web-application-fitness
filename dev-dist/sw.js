56a6f04f1f4abaed6fbd24ff79ad9e54664dff6c
Daniel Hennies <daniel.hennies@stud.uni-hannover.de>
2025-11-25 12:52:22 +0100

    fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-d38da2cf'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/offline.html",
-    "revision": "0.e99tidhbj8g"
+    "revision": "0.n122svcv378"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/offline.html"), {
