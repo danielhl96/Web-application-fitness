653577aa716d74ed9d2d7650a887955ff843a212
danielhl96 <daniel.hennies@t-online.de>
2025-11-30 21:41:27 +0100

    Change border color to blue for email input

--- src/login.jsx
+++ src/login.jsx
@@ -68,7 +68,7 @@ function LoginForm() {
                 border:
                   emailError && emailTouched
                     ? "1px solid red"
-                    : "1px solid #DBEAFE ",
+                    : "1px solid blue ",
               }}
             />
 
