d5429777cd2ed80e6a369306a310a807c4b0670c
danielhl96 <daniel.hennies@t-online.de>
2025-12-03 17:28:24 +0100

    bug

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-d38da2cf'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/offline.html",
-    "revision": "0.hmtkh2pl22"
+    "revision": "0.16bns6ri198"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/offline.html"), {
