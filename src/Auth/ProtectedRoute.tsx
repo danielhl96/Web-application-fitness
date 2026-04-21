1c230a3bafe39570a9daf71a56416d23a6e8d8e4
danielhl96 <daniel.hennies@t-online.de>
2026-04-21 13:43:26 +0200

    feat: update ProtectedRoute to use loading component for loading state

--- src/Auth/ProtectedRoute.tsx
+++ src/Auth/ProtectedRoute.tsx
@@ -1,6 +1,7 @@
 import { JSX, useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { AuthContext } from './AuthProvider';
+import loadingComponente from '../Components/loading';
 
 type ProtectedRouteProps = {
   children: React.ReactNode;
@@ -11,7 +12,7 @@ function ProtectedRoute({ children }: ProtectedRouteProps): JSX.Element {
   if (loading)
     return (
       <div className="min-h-[100dvh] bg-gradient-to-b from-gray-900 to-black flex flex-col items-center justify-center">
-        <span className="loading loading-bars loading-xl"></span>
+        {loadingComponente('Loading...')}
       </div>
     );
   if (!isAuth) return <Navigate to="/login" replace />;
