4ee523b641f9ea448bd22d149d09c09b44a3eecd
danielhl96 <daniel.hennies@t-online.de>
2026-06-10 11:26:28 +0200

    fix: update button component to disable when loading

--- src/shared/Components/button.tsx
+++ src/shared/Components/button.tsx
@@ -18,10 +18,10 @@ const Button = ({
 }: ButtonProps): JSX.Element => {
   return (
     <button
-      disabled={disabled}
+      disabled={disabled || isLoading}
       onClick={onClick}
       className={`relative overflow-hidden text-xs ${w ?? ''} px-4 py-2 rounded-2xl border border-white/30 shadow-xl backdrop-blur-md bg-white/10 text-white flex items-center gap-2 focus:ring-2 focus:ring-blue-400 ${
-        disabled ? 'opacity-60 cursor-not-allowed' : ''
+        disabled || isLoading ? 'opacity-60 cursor-not-allowed' : ''
       }`}
       style={border && !disabled ? { border: `1.5px solid ${border}` } : undefined}
     >
